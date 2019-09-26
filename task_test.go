9ae6542e7a340b53550a0a81686048e9ee5e0ed7
Tabitha Rollins <tabitha@keyneston.com>
2019-09-26 12:34:19 +0100

    Fix test

--- task_test.go
+++ task_test.go
@@ -8,11 +8,28 @@ import (
 
 func TestTask(t *testing.T) {
 	step := NewBuilder()
-	task := NewTask("Foo").Next(NewTask("Bar"))
+	task := NewTask("Foo").Next(NewTask("Bar").Comment("Bar does bar things"))
+
+	expected := `
+{
+    "StartAt": "",
+    "States": {
+        "Bar": {
+            "Type": "Task",
+            "End": true,
+			"Comment": "Bar does bar things"
+        },
+        "Foo": {
+            "Type": "Task",
+            "Next": "Bar"
+        }
+    }
+}
+`
 
 	step.StartAt(task)
 
 	output, err := step.Render()
 	assert.NoError(t, err)
-	assert.Equal(t, string(output), "THIS DOESN'T WORK YET")
+	assert.JSONEq(t, string(output), expected)
 }
